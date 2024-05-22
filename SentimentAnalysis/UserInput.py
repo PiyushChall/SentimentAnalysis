e955fc1a6db366bef4c2f7cff6b6d0367ac75152
PiyushChall <challpiyush79@gmail.com>
2024-05-22 12:34:25 +0530

    Update UserInput.py

--- SentimentAnalysis/UserInput.py
+++ SentimentAnalysis/UserInput.py
@@ -15,7 +15,7 @@ def converting_to_words(sentiment):
     elif sentiment < -0.5:
         return "This is a Strongly Negative sentence :("
     elif -0.5 <= sentiment < 0:
-        return "This is a Positive sentence :("
+        return "This is a Negative sentence :("
     else:
         return "This is a neutral sentence :|"
 
@@ -23,6 +23,7 @@ def converting_to_words(sentiment):
 def main():
     input_text = input("Enter your Sentence: ")
     sentiment_score = analysis(input_text)
+    print(sentiment_score)
     statement = converting_to_words(sentiment_score)
     print(statement)
 
